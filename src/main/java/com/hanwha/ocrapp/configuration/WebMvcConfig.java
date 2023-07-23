c5f4b6dc5d7ea5508626f0a2774cbd0e9be5c247
ChoiJaeEun <poungki0823@gmail.com>
2023-07-23 14:17:41 +0900

    webconfig 수정

--- src/main/java/com/hanwha/ocrapp/configuration/WebMvcConfig.java
+++ src/main/java/com/hanwha/ocrapp/configuration/WebMvcConfig.java
@@ -10,6 +10,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") // 요청을 허용할 경로 설정
                 .allowedOrigins("http://localhost:3000") // 허용할 오리진(클라이언트 주소) 설정
+                .allowedOrigins("http://43.202.101.105:3000") // 허용할 오리진(클라이언트 주소) 설정
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드 설정
                 .allowedHeaders("*") // 허용할 헤더 설정
                 .allowCredentials(false); // 인증정보를 포함할지 여부 설정
